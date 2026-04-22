55eb15d29c3c08c2893c26e6157d33cd8f7013c9
weeaboo <90845477+weaboo699@users.noreply.github.com>
2026-04-22 13:24:52 +0800

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        for(int i = 0; i < (nums.Length); i++){
+            if(map.ContainsKey(target - nums[i])){
+                return [map[target - nums[i]], i];
+            }
+            else{
+                map[nums[i]] = i;
+            }
+        }
+        return new int[0];
+    }
+}
